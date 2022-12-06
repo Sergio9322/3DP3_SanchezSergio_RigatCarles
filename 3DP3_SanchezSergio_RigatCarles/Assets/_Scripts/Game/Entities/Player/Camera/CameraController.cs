b7972ee4026a6b851037b6c764f86a07b0fabe27
Carles Rigat Almansa <carlesrigat@gmail.com>
2022-12-06 23:33:31 +0100

    Cleaned CameraController code

--- 3DP3_SanchezSergio_RigatCarles/Assets/_Scripts/Game/Entities/Player/Camera/CameraController.cs
+++ 3DP3_SanchezSergio_RigatCarles/Assets/_Scripts/Game/Entities/Player/Camera/CameraController.cs
@@ -16,7 +16,7 @@ public class CameraController : MonoBehaviour, IRestartGameElement
 	[SerializeField] float m_InterpolationSpeed = 5f;
 	[SerializeField] bool m_InterpolationActive = true;
 
-	Vector3 l_Direction = Vector3.zero;
+	Vector3 m_Direction = Vector3.zero;
 
 	float m_StartPitch, m_StartYaw;
 	Vector3 m_StartPosition;
@@ -49,58 +49,78 @@ public class CameraController : MonoBehaviour, IRestartGameElement
 	}
 	void LateUpdate()
 	{
-        float l_MouseAxisX = Input.GetAxis("Mouse X");
+        MoveCamera();
+#if UNITY_EDITOR
+		EditorDebugLock();
+#endif
+	}
+
+	void MoveCamera()
+	{
+		float l_MouseAxisX = Input.GetAxis("Mouse X");
         float l_MouseAxisY = Input.GetAxis("Mouse Y");
-
-        l_Direction = m_LookAt.position - transform.position;
-        float l_Distance = l_Direction.magnitude;
-
+        m_Direction = m_LookAt.position - transform.position;
+        float l_Distance = m_Direction.magnitude;
         Vector3 l_DesiredPosition = transform.position;
 
 		if(!m_AngleLocked)
 		{
-			Vector3 l_EulerAngles=transform.eulerAngles;
-			float l_Yaw=(l_EulerAngles.y+180.0f);
-			float l_Pitch=l_EulerAngles.x;
-
-			//TODO: Update Yaw and Pitch
-			l_Yaw += l_MouseAxisX * m_YawRotationalSpeed;
-			l_Pitch += l_MouseAxisY * m_PitchRotationalSpeed;
-
-			l_Pitch = Mathf.Clamp(l_Pitch, m_MinPitch, m_MaxPitch);
-
-			//TODO: Update DesiredPosition
-			l_DesiredPosition.x = m_LookAt.position.x +
-				  Mathf.Sin(l_Yaw * Mathf.Deg2Rad) 
-				* Mathf.Cos(l_Pitch * Mathf.Deg2Rad) 
-				* l_Distance;
-			l_DesiredPosition.y = m_LookAt.position.y +
-				  Mathf.Sin(l_Pitch * Mathf.Deg2Rad)
-				* l_Distance;
-			l_DesiredPosition.z = m_LookAt.position.z +
-				  Mathf.Cos(l_Yaw * Mathf.Deg2Rad)
-				* Mathf.Cos(l_Pitch * Mathf.Deg2Rad)
-				* l_Distance;
-
-			//TODO: Update new direction
-			l_Direction = m_LookAt.position - l_DesiredPosition;
+			ChangeAngle(l_MouseAxisX, l_MouseAxisY, l_DesiredPosition, l_Distance);
 		}
-		l_Direction/=l_Distance;
+		m_Direction/=l_Distance;
 
-		//TODO: Clamp between minDistance and maxDistance. Update desiredPosition.
-		l_Distance = Mathf.Clamp(l_Distance, minCamDist, maxCamDist);
-		l_DesiredPosition = m_LookAt.position - l_Direction * l_Distance;
+		ClampMinAndMaxDistance(ref l_Distance, ref l_DesiredPosition);
+		l_DesiredPosition = BringCameraCloserIfColliding(l_Distance, l_DesiredPosition);
 
-		//TODO: Bring camera closer if colliding with any object.-------------------------------------------------------------------------------------------------------
-		Vector3 l_NewDesiredPosition = m_CameraCollision.GetDesiredPosition(l_Direction, l_Distance, l_DesiredPosition, m_LookAt);
-		l_DesiredPosition = (l_NewDesiredPosition == Vector3.zero) ? l_DesiredPosition : l_NewDesiredPosition;
-
-		transform.forward = l_Direction;
+		transform.forward = m_Direction;
 		transform.position = m_InterpolationActive ? InterpolateNewPosition(l_DesiredPosition) : l_DesiredPosition;
+	}
 
-#if UNITY_EDITOR
-		EditorDebugLock();
-#endif
+	void ChangeAngle(float l_MouseAxisX, float l_MouseAxisY, Vector3 l_DesiredPosition, float l_Distance)
+	{
+		Vector3 l_EulerAngles=transform.eulerAngles;
+		float l_Yaw=(l_EulerAngles.y+180.0f);
+		float l_Pitch=l_EulerAngles.x;
+		UpdateYawAndPitch(l_MouseAxisX, l_MouseAxisY, ref l_Yaw, ref l_Pitch);
+		l_DesiredPosition = GetUpdatedDesiredPosition(l_Distance, l_Yaw, l_Pitch);
+		m_Direction = m_LookAt.position - l_DesiredPosition;
+	}
+
+	void UpdateYawAndPitch(float l_MouseAxisX, float l_MouseAxisY, ref float l_Yaw, ref float l_Pitch)
+	{
+		l_Yaw += l_MouseAxisX * m_YawRotationalSpeed;
+		l_Pitch += l_MouseAxisY * m_PitchRotationalSpeed;
+		l_Pitch = Mathf.Clamp(l_Pitch, m_MinPitch, m_MaxPitch);
+	}
+
+	Vector3 GetUpdatedDesiredPosition(float l_Distance, float l_Yaw, float l_Pitch)
+	{
+		Vector3 l_DesiredPosition = Vector3.zero;
+		l_DesiredPosition.x = m_LookAt.position.x +
+			  Mathf.Sin(l_Yaw * Mathf.Deg2Rad) 
+			* Mathf.Cos(l_Pitch * Mathf.Deg2Rad) 
+			* l_Distance;
+		l_DesiredPosition.y = m_LookAt.position.y +
+			  Mathf.Sin(l_Pitch * Mathf.Deg2Rad)
+			* l_Distance;
+		l_DesiredPosition.z = m_LookAt.position.z +
+			  Mathf.Cos(l_Yaw * Mathf.Deg2Rad)
+			* Mathf.Cos(l_Pitch * Mathf.Deg2Rad)
+			* l_Distance;
+		return l_DesiredPosition;
+	}
+
+	void ClampMinAndMaxDistance(ref float l_Distance, ref Vector3 l_DesiredPosition)
+	{
+		l_Distance = Mathf.Clamp(l_Distance, minCamDist, maxCamDist);
+		l_DesiredPosition = m_LookAt.position - m_Direction * l_Distance;
+	}
+
+	Vector3 BringCameraCloserIfColliding(float l_Distance, Vector3 l_DesiredPosition)
+	{
+		Vector3 l_NewDesiredPosition = m_CameraCollision.GetDesiredPosition(m_Direction, l_Distance, l_DesiredPosition, m_LookAt);
+		l_DesiredPosition = (l_NewDesiredPosition == Vector3.zero) ? l_DesiredPosition : l_NewDesiredPosition;
+		return l_DesiredPosition;
 	}
 
 	Vector3 InterpolateNewPosition(Vector3 l_DesiredPosition)

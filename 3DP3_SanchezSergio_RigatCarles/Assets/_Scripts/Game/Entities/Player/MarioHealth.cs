65d2a9e5e5bbc2f47a9cc4572738b68e767337b1
Carles Rigat Almansa <carlesrigat@gmail.com>
2022-12-07 18:08:14 +0100

    Animation to refill healthBar after restarting done
    TODO: Fix transition on loosing a life

--- 3DP3_SanchezSergio_RigatCarles/Assets/_Scripts/Game/Entities/Player/MarioHealth.cs
+++ 3DP3_SanchezSergio_RigatCarles/Assets/_Scripts/Game/Entities/Player/MarioHealth.cs
@@ -8,11 +8,13 @@ public class MarioHealth : MonoBehaviour, IRestartGameElement
     [SerializeField] int m_Lifes = 3;
     [SerializeField] int m_Health = 8;
     [SerializeField] int m_MaxHealth = 8;
+    [SerializeField] GameObject m_HealthBar;
+    float m_RespawnAnimationTime = 7f;
     [SerializeField] Image m_HealthBarImage, m_HealthBarBackgroundOverlay;
     float m_BackgroundAlpha = 0.5f;
     [SerializeField] Gradient m_HealthBarColour;
 
-    [SerializeField] float m_DelayBetweenLife = 0.2f;
+    [SerializeField] float m_DelayBetweenLife = 1f;
     [SerializeField] float m_RespawnDelay = 5f;
 
     void Start()
@@ -24,6 +26,7 @@ public class MarioHealth : MonoBehaviour, IRestartGameElement
 
     public void TakeDamage(int damage)
     {
+        m_HealthBar.SetActive(true);
         StartCoroutine(UpdateHealthBar(damage));
     }
 
@@ -44,8 +47,8 @@ public class MarioHealth : MonoBehaviour, IRestartGameElement
             m_HealthBarImage.color = m_HealthBarColour.Evaluate(m_HealthBarImage.fillAmount);
             m_HealthBarBackgroundOverlay.color = m_HealthBarColour.Evaluate(m_HealthBarImage.fillAmount);
             m_HealthBarBackgroundOverlay.color = new Color(m_HealthBarBackgroundOverlay.color.r, m_HealthBarBackgroundOverlay.color.g, m_HealthBarBackgroundOverlay.color.b, m_BackgroundAlpha * m_HealthBarImage.fillAmount);
-            SubstractDamage();
             yield return new WaitForSeconds(m_DelayBetweenLife);
+            SubstractDamage();
         }
     }
 
@@ -55,8 +58,7 @@ public class MarioHealth : MonoBehaviour, IRestartGameElement
         if (m_Health <= 0)
         {
             m_Lifes--;
-            m_Health = m_MaxHealth;
-            RespawnHealthBar();
+            GameController.GetGameController().RestartGame();
         }
     }
 
@@ -68,20 +70,24 @@ public class MarioHealth : MonoBehaviour, IRestartGameElement
     IEnumerator RespawnHealthBarCoroutine()
     {
         yield return new WaitForSeconds(m_RespawnDelay);
+        m_HealthBar.GetComponent<Animation>().Play("RespawnHealthBarAnimation");
+        yield return new WaitForSeconds(m_RespawnDelay/2);
         int l_LifesToAdd = m_MaxHealth - m_Health;
         for (int i = 0; i < l_LifesToAdd; i++)
         {
-            m_HealthBarImage.fillAmount += 1 / m_MaxHealth;
+            m_HealthBarImage.fillAmount += 1f / m_MaxHealth;
             m_HealthBarImage.color = m_HealthBarColour.Evaluate(m_HealthBarImage.fillAmount);
             m_HealthBarBackgroundOverlay.color = m_HealthBarColour.Evaluate(m_HealthBarImage.fillAmount);
             m_HealthBarBackgroundOverlay.color = new Color(m_HealthBarBackgroundOverlay.color.r, m_HealthBarBackgroundOverlay.color.g, m_HealthBarBackgroundOverlay.color.b, m_BackgroundAlpha * m_HealthBarImage.fillAmount);
             yield return new WaitForSeconds(m_DelayBetweenLife);
         }
+        yield return new WaitForSeconds(m_RespawnAnimationTime);
+        m_HealthBar.SetActive(false);
+        m_Health = m_MaxHealth;
     }
 
     public void RestartGame()
     {
-        m_Health = m_MaxHealth;
         RespawnHealthBar();
     }
 }

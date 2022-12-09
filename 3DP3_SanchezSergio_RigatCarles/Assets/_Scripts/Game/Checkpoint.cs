ed73cf7fd38408741f873ae6be2cb90333af6860
Carles Rigat Almansa <carlesrigat@gmail.com>
2022-12-09 02:13:56 +0100

    Checkpoint polished

--- 3DP3_SanchezSergio_RigatCarles/Assets/_Scripts/Game/Checkpoint.cs
+++ 3DP3_SanchezSergio_RigatCarles/Assets/_Scripts/Game/Checkpoint.cs
@@ -2,7 +2,37 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Animation))]
 public class Checkpoint : MonoBehaviour
 {
     public Transform m_RespawnPosition;
+    bool m_FirstTime = true;
+    [SerializeField] AudioSource m_CheckpointAudioSource;
+    [SerializeField] Material m_ActivatedMaterial;
+    [SerializeField] SkinnedMeshRenderer m_MeshRenderer;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            if (m_FirstTime)
+            {
+                m_FirstTime = false;
+                FirstTimeActivation();
+            }
+            else RegularActivation();
+        }
+    }
+
+    void RegularActivation()
+    {
+        GetComponent<Animation>().Play();
+        m_CheckpointAudioSource.Play();
+    }
+
+    void FirstTimeActivation()
+    {
+        m_MeshRenderer.material = m_ActivatedMaterial;
+        RegularActivation();
+    }
 }

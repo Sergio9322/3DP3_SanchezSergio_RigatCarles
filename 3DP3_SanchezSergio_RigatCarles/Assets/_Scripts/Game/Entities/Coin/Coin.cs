adef8bfa0728c25d1cca012c384759027ef52863
Carles Rigat Almansa <carlesrigat@gmail.com>
2022-12-09 01:30:07 +0100

    Audio and Particles added to Coin. Made fall from sky to avoid jump animation stuck

--- 3DP3_SanchezSergio_RigatCarles/Assets/_Scripts/Game/Entities/Coin/Coin.cs
+++ 3DP3_SanchezSergio_RigatCarles/Assets/_Scripts/Game/Entities/Coin/Coin.cs
@@ -6,6 +6,9 @@ public class Coin : MonoBehaviour, IRestartGameElement, IPickable
 {
     bool m_HasStartedDeactivation = false;
     float m_DeactivationSpeed = 4.0f;
+    float m_TimeToDeactivate = 1f;
+    [SerializeField] AudioSource m_CoinAudioSource;
+    [SerializeField] ParticleSystem m_CoinParticleSystem;
     void Start()
     {
         GameController.GetGameController().AddRestartGameElement(this);
@@ -15,6 +18,8 @@ public class Coin : MonoBehaviour, IRestartGameElement, IPickable
     {
         if (!m_HasStartedDeactivation)
         {
+            m_CoinAudioSource.Play();
+            m_CoinParticleSystem.Play();
             StartCoroutine(Deactivate());
             GameController.GetGameController().GetDependencyInjector().GetDependency<IScoreManager>().addPoints(1);
         }
@@ -30,7 +35,7 @@ public class Coin : MonoBehaviour, IRestartGameElement, IPickable
             transform.localScale = new Vector3(l_Scale, l_Scale, l_Scale);
             yield return null;
         }
-        
+        yield return new WaitForSeconds(m_TimeToDeactivate);
         gameObject.SetActive(false);
         m_HasStartedDeactivation = false;
     }
